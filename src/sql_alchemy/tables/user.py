1b3ae1bc8626814ccb0adbf12f240dd76ec15a97
Pulkit Singh Baliyan <duke79@users.noreply.github.com>
2018-10-05 23:37:16 +0530

    Update user.py

--- src/sql_alchemy/tables/user.py
+++ src/sql_alchemy/tables/user.py
@@ -13,15 +13,7 @@ class User(db.Model):
     photo_url = db.Column(db.String(2013), nullable=True)
     email = db.Column(db.String(200), nullable=True)
     firebase_uid = db.Column(db.String(100), nullable=True)
-    admin = db.Column(db.Boolean, nullable=False, server_default=expression.false())
-    # created_at = db.Column(db.DateTime, default=db.func.now())
-    # updated_at = db.Column(db.DateTime, default=db.func.now())
-    ## https://bitbucket.org/zzzeek/sqlalchemy/issues/3444/sqlalchemy-does-not-emit-server_onupdate
-    created_at = db.Column(db.TIMESTAMP(), nullable=False,
-                           server_default=db.text('CURRENT_TIMESTAMP'))
-    updated_at = db.Column(db.TIMESTAMP(), nullable=False,
-                           server_default=db.text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
-                           )
+    admin = db.Column(db.Boolean, nullable=False, server_default=expression.false())    
     # children = relationship("user_categories")
 
     def __repr__(self):
