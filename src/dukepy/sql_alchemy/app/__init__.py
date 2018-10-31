c01610069194eda6ef81a61ef326d4b88ec6ce8d
Pulkit Singh Baliyan <duke79@users.noreply.github.com>
2018-10-31 20:47:43 +0530

    Update __init__.py

--- src/dukepy/sql_alchemy/app/__init__.py
+++ src/dukepy/sql_alchemy/app/__init__.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from dukepy.config import Config
 from dukepy.sql_alchemy.tables.alchemy_base import AlchemyBase, db_uri, db_session
@@ -17,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 if config["debug"]:
     app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app, model_class=AlchemyBase)
-# db.create_all() # No use until the model classes are imported. Use scripts.create_db_schema instead.
+migrate = Migrate(app, db)
 
 """ Views """
 # from app.views import
