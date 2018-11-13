089858ad800e17ded610ff2ed0b90825a87d918a
duke79 <pulkitsingh01@gmail.com>
2018-11-13 08:58:21 +0530

    websocketd | doesn't work with pipenv

--- src/fire.bat
+++ src/fire.bat
@@ -3,6 +3,15 @@
 set init_dir=%cd%
 cd %~dp0
 
-pipenv run python cli\fire\fire_cli.py %*
+python cli\fire\fire_cli.py %*
+
+:loop
+
+set /p INPUT=
+
+REM https://stackoverflow.com/a/1420981/973425
+python cli\fire\fire_cli.py %INPUT% 2>&1
+
+goto loop
 
 cd %init_dir%
