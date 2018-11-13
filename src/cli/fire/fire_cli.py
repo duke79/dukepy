2b4f7e565388c70044ab446b5ca9a587118d880d
duke79 <pulkitsingh01@gmail.com>
2018-11-13 21:58:22 +0530

    fire | json arg

--- src/cli/fire/fire_cli.py
+++ src/cli/fire/fire_cli.py
@@ -3,6 +3,8 @@ import sys
 
 import fire
 
+def json_dump(obj):
+    print(json.dumps(obj, indent=4, sort_keys=True, default=str))  # https://stackoverflow.com/a/11875813/973425
 
 class Root():
     """
@@ -13,9 +15,17 @@ class Root():
     def __init__(self):
         pass
 
+    def echo(self, arg):
+        """
+        JSON arg Tested : echo '{"a":"123", "tois":{"moins":12, "hil":["hodor", "mind"]}}'
+        :param arg:
+        :return:
+        """
+        json_dump(arg)
+
     def history(self):
         for cmd in Root.cmd_history:
-            print(json.dumps(cmd, indent=4, sort_keys=True, default=str))  # https://stackoverflow.com/a/11875813/973425
+            json_dump(cmd)
 
 
 def main():
