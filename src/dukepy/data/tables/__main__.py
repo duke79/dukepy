a7dbba6d417c5551bf78110375e2e76c60422bf7
Pulkit Singh Baliyan <duke79@users.noreply.github.com>
2018-11-18 17:32:32 +0530

    Create __main__.py

---
+++ src/dukepy/data/tables/__main__.py
@@ -0,0 +1,34 @@
+from .incident_report import IncidentReport
+from data.common.base import db_session
+
+
+def create_people():
+	bruno = IncidentReport("Bruno Krebs")
+	john = IncidentReport("John Doe")
+
+	session = db_session()
+	session.add(bruno)
+	session.add(john)
+	session.commit()
+	session.close()
+
+
+def get_people():
+	session = db_session()
+	people_query = session.query(IncidentReport)
+	session.close()
+	return people_query.all()
+
+
+def main():
+	people = get_people()
+	if len(people) == 0:
+		create_people()
+	people = get_people()
+
+	for person in people:
+		print('{0} was born'.format(person.name))
+
+
+if __name__ == "__main__":
+	main()
