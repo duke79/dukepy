3df0c0a0df2982693239792749465b7d3b5b8f42
duke79 <pulkitsingh01@gmail.com>
2018-11-24 20:37:38 +0530

    task_manager | derivation

--- src/dukepy/task_manager.py
+++ src/dukepy/task_manager.py
@@ -1,4 +1,15 @@
-from multiprocessing import Process
+from multiprocessing import Process, Event
+
+
+class Task(Process):
+    def __init__(self, *args, **kwargs):
+        Process.__init__(self, *args, **kwargs)
+        self._queue = []
+        self._target = kwargs["target"]
+        self._args = kwargs["args"]
+
+    def run(self):
+        self._target(*self._args)
 
 
 def task1(args):
@@ -12,10 +23,12 @@ def task2(args):
 
 
 def main():
-    p1 = Process(target=task1, args=("hello",))
-    p2 = Process(target=task2, args=("hello",))
+    p1 = Task(target=task1, args=("hello",))
+    p2 = Task(target=task2, args=("hello",))
     p1.start()
     p2.start()
+    # p1.terminate()
+    # p2.terminate()
 
 
 if __name__ == "__main__":
