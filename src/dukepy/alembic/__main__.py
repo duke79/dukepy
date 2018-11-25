961406029622db2acb68ba301102e9be9b6c9e9b
Pulkit Singh Baliyan <duke79@users.noreply.github.com>
2018-11-25 12:55:05 +0530

    Create __main__.py

---
+++ src/dukepy/alembic/__main__.py
@@ -0,0 +1,30 @@
+from incident_report import IncidentReport
+from data.common.base import db_session
+
+
+def create_people():
+	bruno = IncidentReport("Bruno Krebs", "abc")
+	john = IncidentReport("John Doe", "bcd")
+	bruno.save()
+	john.save()
+
+
+def get_people():
+	session = db_session()
+	people_query = session.query(IncidentReport)
+	session.close()
+	return people_query.all()
+
+
+def main():
+	people = get_people()
+	if len(people) == 0:
+		create_people()
+	people = get_people()
+
+	for person in people:
+		print('{0} was born'.format(person.name))
+
+
+if __name__ == "__main__":
+	main()
