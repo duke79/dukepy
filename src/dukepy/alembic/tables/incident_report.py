b72eaf3f2ae0c633be6765b6b782c6ca578973cf
Pulkit Singh Baliyan <duke79@users.noreply.github.com>
2018-11-25 12:56:11 +0530

    Create incident_report.py

---
+++ src/dukepy/alembic/tables/incident_report.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String
+
+# from app import db
+from data.common import Base
+# from flask_sqlalchemy import Model
+from data.common.mixin import Mixin
+
+
+class IncidentReport(Mixin, Base):
+	name = Column(String(200), nullable=True)
+	trigram = Column(String(200), nullable=True)
+	machine = Column(String(200), nullable=True)
+	# hobby = Column(String(200), nullable=True)
+
+	def __init__(self, name, trigram):
+		self.name = name
+		self.trigram = name
