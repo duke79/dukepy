e4d55828f2848c66f06def778f5c0395cf7d5c85
Pulkit Singh Baliyan <duke79@users.noreply.github.com>
2018-12-06 09:34:40 +0530

    Update traces.py

--- src/dukepy/traces.py
+++ src/dukepy/traces.py
@@ -1,18 +1,48 @@
+from __future__ import print_function
+
 import traceback
-from dukepy.config import Config
+
+import sys
+import json as JSON
+
+import os
+
+
+def eprint(*args, **kwargs):
+	print(*args, file=sys.stderr, **kwargs)
 
 
 def print_exception_traces(e):
-    config = Config()
-    if config["stacktrace"]:
-        print(traceback.format_exc())
-    else:
-        print(e)
+	try:
+		if os.environ.get("stacktrace"):
+			eprint(traceback.format_exc())
+		elif sys.argv["stacktrace"]:
+			eprint(e)
+	except Exception as ee:  # LOL !
+		eprint(e)
+		pass
 
 
-if __name__ == "__main__":
-    mydict = dict()
-    try:
-        print(mydict["sdf"])
-    except Exception as e:
-        print_exception_traces(e)
+def print_for_cli(e, json=False):
+	try:
+		if os.environ["print_for_cli"]:
+			if not json:
+				print(e)
+			else:
+				print(
+					JSON.dumps(e, indent=4, sort_keys=True, default=str))  # https://stackoverflow.com/a/11875813/973425
+	except Exception as e:
+		# print_exception_traces(e)
+		pass
+
+
+class TracePrints(object):
+	def __init__(self):
+		self.stdout = sys.stdout
+
+	def write(self, s):
+		self.stdout.write("Writing %r\n" % s)
+		import traceback
+		traceback.print_stack(file=self.stdout)
+
+# sys.stdout = TracePrints()
